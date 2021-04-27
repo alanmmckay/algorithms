3c6fab09df7dbf50377feef47db6f461bc98447f
Alan McKay <alanmmckay@gmail.com>
2021-04-27 13:38:40 -0500

    Reordered class declarations. Deleted extraneous comments

--- nummaze.py
+++ nummaze.py
@@ -29,7 +29,137 @@ class Directions(Enum):
         yield self.LEFT
         yield self.RIGHT
         
+            
+class Vertex(object):
+    def __init__(self,identifier,label = None):
+        self.identifier = identifier
+        self.label = label
+        self.inNeighbors = []
+        self.outNeighbors = []
+        self.status = Status.NEW
+        self.clock = 0
         
+    def getID(self):
+        return self.identifier
+    
+    def getLabel(self):
+        return self.label
+    
+    def setLabel(self,label):
+        self.label = label
+        return True
+    
+    def getStatus(self):
+        return self.status
+    
+    def setStatus(self,status):
+        self.status = status
+        return True
+    
+    def incrementStatus(self):
+        if self.status == Status.FINISHED:
+            return False
+        else:
+            self.status = self.status + 1
+            return True
+    
+    def addInNeighbor(self,vertex):
+        if vertex not in self.inNeighbors:
+            self.inNeighbors.append(vertex)
+            return True
+        else:
+            return False
+    
+    def getInNeighbors(self):
+        return self.inNeighbors
+    
+    def addOutneighbor(self,vertex):
+        if vertex not in self.outNeighbors:
+            self.outNeighbors.append(vertex)
+            return True
+        else:
+            return False
+        
+    def getOutNeighbors(self):
+        return self.outNeighbors
+    
+    def setClock(self,time):
+        self.clock = time
+        return True
+    
+    def getClock(self):
+        return self.clock
+    
+
+class Graph(object):
+    def __init__(self):
+        self.vertices = []
+        self.vertexIds = []
+        self.edges = []
+        
+        self.graphQueue = []
+        self.edge_str = ""
+        self.node_str = ""
+    
+    def createVertex(self,identifier,label=None):
+        if identifier not in self.vertexIds:
+            vertex = Vertex(identifier,label)
+            self.vertices.append(vertex)
+            self.vertexIds.append(identifier)
+            return vertex
+        else:
+            return False
+    
+    def createEdge(self,vertexA,vertexB,weight = None):
+        if vertexA.getID() in self.vertexIds and vertexB.getID() in self.vertexIds:
+            edge = (vertexA,vertexB,weight)
+            if edge not in self.edges:
+                self.edges.append(edge)
+                vertexA.addOutneighbor(vertexB)
+                vertexB.addInNeighbor(vertexA)
+                return edge
+            else:
+                return False
+        else:
+            return False
+        
+    def getVertex(self,identifier):
+        for vertex in self.vertices:
+            if vertex.getID() == identifier:
+                return vertex
+        return False
+    
+    def getVertices(self):
+        return self.vertices
+    
+    def vertexCheck(self,identifier):
+        if identifier in vertexIds:
+            return True
+        else:
+            return False
+        
+    def renderGraph(self,vertex):
+        self.graphQueue = [vertex]
+        self.edge_str = ""
+        self.node_str = ""
+        return self.renderGraphDFS(vertex)
+    
+    def renderGraphDFS(self,vertex):
+        self.node_str += '"'+str(vertex.getID())+'"'
+        self.node_str += ' [label="'+str(vertex.getID())
+        self.node_str += '",shape=circle,fixedsize=true,fontsize=18,width=1]\n'
+        self.graphQueue.append(vertex)
+        #vertex.incrementStatus()
+        for outNeighbor in vertex.getOutNeighbors():
+            if outNeighbor not in self.graphQueue:
+                self.renderGraphDFS(outNeighbor)
+            self.edge_str += '"'+str(vertex.getID())+'" -> "' + str(outNeighbor.getID())+'"'
+            self.edge_str += ' [penwidth=1]\n'
+        #vertex.incrementStatus()
+        primer_str = 'digraph D {\n'+self.node_str+'\n'+self.edge_str+'}'
+        return primer_str + self.node_str + self.edge_str
+    
+
 class Maze(object):
     def __init__(self):
         self.rows = 0
@@ -40,6 +170,7 @@ class Maze(object):
         self.startVertex = Vertex(None)
         self.endCoordinate = ()
         self.graphQueue = []
+        self.callcount = 0
         
     def importFrom(self,fileName):
         try:
@@ -131,6 +262,7 @@ class Maze(object):
         self.generateGraphDFS(self.startVertex)
         
     def generateGraphDFS(self,vertex):
+        self.callcount = self.callcount + 1
         coordinate = (vertex.getID(),vertex.getLabel())
         neighbors = self.traversalsFrom(coordinate)
         for neighbor in neighbors:
@@ -141,22 +273,22 @@ class Maze(object):
             self.graph.createEdge(vertex,self.graph.getVertex(neighbor[0]))
             
             
-    def findRoute(self,source,destination):
-        dfsQueue = [source]
-        count = 0
+    '''def findRoute(self,sourceCoordinate,destinationCoordinate):
+        self.graph = Graph()
+        self.startingVertex = self.graph.createVertex(sourceCoordinate[0],self.sourceCoordinate[1])
+        dfsQueue = [self.startingVertex]
+        routeQueue = [self.startingVertex.getID()]
         while dfsQueue:
             vertex = dfsQueue.pop()
-            if vertex == destination:
-                return count
+            if vertex.getID() == destinationCoordinate:
+                return vertex.getClock() #perhaps move
             else:
                 vertex.incrementStatus()
-                for neighbor in vertex.getOutNeighbors():
+                neighborCoordinates = self.traversalsFrom(vertex.getID())
+                for coordinate in neighborCoordinates():
                     if neighbor.getStatus() == Status.NEW:
                         dfsQueue.append(neighbor)
-            count = count + 1
-        return False
-        
-            
+        return False'''
         
             
     def getGraph(self):
@@ -166,129 +298,9 @@ class Maze(object):
         return self.graph.renderGraph(self.startVertex)
         
      
-            
-class Vertex(object):
-    def __init__(self,identifier,label = None):
-        self.identifier = identifier
-        self.label = label
-        self.inNeighbors = []
-        self.outNeighbors = []
-        self.status = Status.NEW
-        
-    def getID(self):
-        return self.identifier
-    
-    def getLabel(self):
-        return self.label
-    
-    def setLabel(self,label):
-        self.label = label
-        return True
-    
-    def getStatus(self):
-        return self.status
-    
-    def setStatus(self,status):
-        self.status = status
-        return True
-    
-    def incrementStatus(self):
-        if self.status == Status.FINISHED:
-            return False
-        else:
-            self.status = self.status + 1
-            return True
-    
-    def addInNeighbor(self,vertex):
-        if vertex not in self.inNeighbors:
-            self.inNeighbors.append(vertex)
-            return True
-        else:
-            return False
-    
-    def getInNeighbors(self):
-        return self.inNeighbors
-    
-    def addOutneighbor(self,vertex):
-        if vertex not in self.outNeighbors:
-            self.outNeighbors.append(vertex)
-            return True
-        else:
-            return False
-        
-    def getOutNeighbors(self):
-        return self.outNeighbors
-    
 
-class Graph(object):
-    def __init__(self):
-        self.vertices = []
-        self.vertexIds = []
-        self.edges = []
-        
-        self.graphQueue = []
-        self.edge_str = ""
-        self.node_str = ""
     
-    def createVertex(self,identifier,label=None):
-        if identifier not in self.vertexIds:
-            vertex = Vertex(identifier,label)
-            self.vertices.append(vertex)
-            self.vertexIds.append(identifier)
-            return vertex
-        else:
-            return False
-    
-    def createEdge(self,vertexA,vertexB,weight = None):
-        if vertexA.getID() in self.vertexIds and vertexB.getID() in self.vertexIds:
-            edge = (vertexA,vertexB,weight)
-            if edge not in self.edges:
-                self.edges.append(edge)
-                vertexA.addOutneighbor(vertexB)
-                vertexB.addInNeighbor(vertexA)
-                return edge
-            else:
-                return False
-        else:
-            return False
-        
-    def getVertex(self,identifier):
-        for vertex in self.vertices:
-            if vertex.getID() == identifier:
-                return vertex
-        return False
-    
-    def getVertices(self):
-        return self.vertices
-    
-    def vertexCheck(self,identifier):
-        if identifier in vertexIds:
-            return True
-        else:
-            return False
-        
-    def renderGraph(self,vertex):
-        self.graphQueue = [vertex]
-        self.edge_str = ""
-        self.node_str = ""
-        return self.renderGraphDFS(vertex)
-    
-    def renderGraphDFS(self,vertex):
-        self.node_str += '"'+str(vertex.getID())+'"'
-        self.node_str += ' [label="'+str(vertex.getID())
-        self.node_str += '",shape=circle,fixedsize=true,fontsize=18,width=1]\n'
-        self.graphQueue.append(vertex)
-        #vertex.incrementStatus()
-        for outNeighbor in vertex.getOutNeighbors():
-            if outNeighbor not in self.graphQueue:
-                self.renderGraphDFS(outNeighbor)
-            self.edge_str += '"'+str(vertex.getID())+'" -> "' + str(outNeighbor.getID())+'"'
-            self.edge_str += ' [penwidth=1]\n'
-        #vertex.incrementStatus()
-        primer_str = 'digraph D {\n'+self.node_str+'\n'+self.edge_str+'}'
-        return primer_str + self.node_str + self.edge_str
-    
-    
+   
         
 
 maze = Maze()
@@ -297,65 +309,8 @@ maze.importFrom("SmallB.txt")
 print(len(maze.matrix[0]))
 #print(maze.displayMatrix())
 
-#maze.generateGraph()
+maze.generateGraph()
+print(maze.callcount)
 
 #print(maze.renderGraph())
-#dfs logic
-'''
-coordinates = maze.getCoordinates()
-print(coordinates)
-print(maze.traversalsFrom(coordinates[0]))
 
-g = Graph()
-start = coordinates[0]
-end = coordinates[-1]
-
-print(start)
-print(end)
-created = [start]
-startingVertex = g.createVertex(start[0],start[1])
-print(start)
-
-def formGraph(vertex):
-    coordinate = (vertex.getID(),vertex.getLabel())
-    neighbors = maze.traversalsFrom(coordinate)
-    for neighbor in neighbors:
-        if neighbor not in created:
-            newVertex = g.createVertex(neighbor[0],neighbor[1])
-            created.append(neighbor)
-            formGraph(newVertex)
-        g.createEdge(vertex,g.getVertex(neighbor[0]))
-        
-formGraph(startingVertex)
-'''
-
-#graph logic
-'''
-discovered = []
-edge_str = ""
-node_str = ""
-
-def generateGraph(vertex):
-    global discovered
-    global edge_str
-    global node_str
-    node_str += '"'+str(vertex.getID())+'"'
-    node_str += ' [label ="'+str(vertex.getID())
-    node_str += '",shape=circle,fixedsize=true,fontsize=18,width=1]\n'
-    discovered.append(vertex)
-    vertex.incrementStatus()
-    for outNeighbor in vertex.getOutNeighbors():
-        if outNeighbor not in discovered:
-            generateGraph(outNeighbor)
-        edge_str += '"'+str(vertex.getID()) + '" -> "' + str(outNeighbor.getID())+'"'
-        edge_str += ' [penwidth=1]\n'
-    vertex.incrementStatus()
-
-#generateGraph(startingVertex)
-    #create queue 
-    #vertex = g.createVertex(pair[0],pair[1])
-
-print("digraph D { \n "+node_str+"\n"+edge_str+"}")
-
-print(len(g.vertices))
-'''
