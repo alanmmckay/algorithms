e05dcc294d99563ef953eb2751ba48792f98ee01
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2023-11-29 13:55:20 -0600

    Implemented logic for the accessor and mutator methods. Haven't done any testing yet to ensure integrity. Need to put in place a destructor as well.

--- C_programs/queue.c
+++ C_programs/queue.c
@@ -35,24 +35,61 @@ int peek_IntegerQueue(struct IntegerQueue* this){
     return this -> collection[ this -> front];
 }
 
+
 void enqueue_IntegerQueue(struct IntegerQueue* this, int new_item){
-    // increment rear
-    // place value
-    // increment quantity
-    // check size
+    this -> rear = (( this -> rear) + 1) % ( this -> limit);
+    this -> collection[ this -> rear] = new_item;
+    this -> quantity = this -> quantity + 1;
+    if( this -> quantity >= this -> limit){
+        this -> limit = ( this -> limit) * 1.5;
+        int *new_array = (int*)calloc( this -> limit, sizeof(int));
+        //Fill the first value of the new_array:
+        new_array[0] = this -> collection[ this -> front];
+        //Grab the next logical index:
+        int c_index = ( this -> front) + 1;
+        //Set the next index of the new array:
+        int n_index = 1;
+        //Cycle through the current set of logical indeces: I.e.,
+        //      r  f                  f              r
+        // [ 50 60 10 20 30 40 ] -> [ 10 20 30 40 50 60 x x x]
+        while(c_index != this -> front){
+            new_array[n_index] = this -> collection[c_index];
+            c_index = (c_index + 1) % ( this -> quantity);
+            n_index += 1;
+        }
+        //Point the collection to the new_array:
+        this -> collection = new_array;
+        //reset the front and rear pointers:
+        this -> front = 0;
+        this -> rear = ( this -> quantity) - 1;
+    }
 }
+
 
 int dequeue_IntegerQueue(struct IntegerQueue* this){
     if( this -> quantity <= 0){
         printf("Error in IntegerQueue dequeue: Queue has no elements!");
         exit(1);
     }
-    // remember value at front
-    // increment front
-    // decrement quantity
-    // return remembered value
-    return 0;
+    int value = this -> collection[ this -> front];
+    this -> front = (( this -> front) + 1) % ( this -> limit);
+    this -> quantity = ( this -> quantity) - 1;
+    //return this -> collection[( this -> front) - 1]
+    return value;
 }
+
+
+void print_IntegerQueue(struct IntegerQueue* this){
+    printf("Front -> ");
+    int val;
+    for(int i = this -> front; i % ( this -> limit) != this -> rear; i++){
+        val = this -> collection[i % ( this -> limit)];
+        printf("%d, ", val);
+    }
+    printf("%d", this -> collection[ this -> rear]);
+    printf(" <- Rear\n");
+}
+
 
 struct IntegerQueue* create_IntegerQueue(int new_limit){
     struct IntegerQueue* newQueue = (struct IntegerQueue*)malloc
@@ -63,7 +100,7 @@ struct IntegerQueue* create_IntegerQueue(int new_limit){
         newQueue -> front = 0;
         newQueue -> rear = 0;
         newQueue -> collection = (int*)calloc(new_limit,sizeof(int));
-        //newQueue -> print =
+        newQueue -> print = print_IntegerQueue;
         newQueue -> dequeue = dequeue_IntegerQueue;
         newQueue -> enqueue = enqueue_IntegerQueue;
         newQueue -> peek = peek_IntegerQueue;
