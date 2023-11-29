2ee2f611c23c943ad1bc4c71616f2b95ed818761
Alan <alanmmckay@gmail.com>
2023-11-29 17:46:06 -0600

    Fixed minor logical errors and implemented test functions. This circular queue seems to work as intended.

--- C_programs/queue.c
+++ C_programs/queue.c
@@ -14,6 +14,8 @@ struct IntegerQueue{
     int (*peek)(struct IntegerQueue*);
 };
 
+//   f&r
+// [ x x x x x x x x x x ]
 //   f       r
 // [ 3 5 6 7 0 x x x x x ]
 //             f       r
@@ -29,7 +31,7 @@ struct IntegerQueue{
 
 int peek_IntegerQueue(struct IntegerQueue* this){
     if( this -> quantity <= 0){
-        printf("Error on IntegerQueue peek: Stack has no elements!");
+        printf("Error on IntegerQueue peek: Queue has no elements!");
         exit(1);
     }
     return this -> collection[ this -> front];
@@ -74,6 +76,10 @@ int dequeue_IntegerQueue(struct IntegerQueue* this){
     int value = this -> collection[ this -> front];
     this -> front = (( this -> front) + 1) % ( this -> limit);
     this -> quantity = ( this -> quantity) - 1;
+    if( this -> quantity <= 0){
+        this -> front = 0;
+        this -> rear = -1;
+    }
     //return this -> collection[( this -> front) - 1]
     return value;
 }
@@ -81,15 +87,32 @@ int dequeue_IntegerQueue(struct IntegerQueue* this){
 
 void print_IntegerQueue(struct IntegerQueue* this){
     printf("Front -> ");
-    int val;
-    for(int i = this -> front; i % ( this -> limit) != this -> rear; i++){
-        val = this -> collection[i % ( this -> limit)];
-        printf("%d, ", val);
+    if( this -> quantity > 0){
+        int val;
+        for(int i = this -> front; i % ( this -> limit) != this -> rear; i++){
+            val = this -> collection[i % ( this -> limit)];
+            printf("%d, ", val);
+        }
+        if( this -> quantity > 0){
+            printf("%d", this -> collection[ this -> rear]);
+        }
     }
-    printf("%d", this -> collection[ this -> rear]);
     printf(" <- Rear\n");
 }
 
+void print_collection(struct IntegerQueue* this){
+    printf("[ ");
+    for(int i = 0; i < this -> limit; i++){
+        if(i == this -> front){
+            printf("Front -> ");
+        }
+        if(i == this -> rear){
+            printf("Rear -> ");
+        }
+        printf("%d | ", this -> collection[i]);
+    }
+    printf(" ]\n\n");
+}
 
 struct IntegerQueue* create_IntegerQueue(int new_limit){
     struct IntegerQueue* newQueue = (struct IntegerQueue*)malloc
@@ -98,7 +121,7 @@ struct IntegerQueue* create_IntegerQueue(int new_limit){
         newQueue -> limit = new_limit;
         newQueue -> quantity = 0;
         newQueue -> front = 0;
-        newQueue -> rear = 0;
+        newQueue -> rear = -1;
         newQueue -> collection = (int*)calloc(new_limit,sizeof(int));
         newQueue -> print = print_IntegerQueue;
         newQueue -> dequeue = dequeue_IntegerQueue;
@@ -109,7 +132,84 @@ struct IntegerQueue* create_IntegerQueue(int new_limit){
 }
 
 
-int main(){
+void test_enqueue(struct IntegerQueue* queue, int val){
+    printf("Enqueue operation: \n");
+    printf("   pre-enqueue-quantity: %d\n", queue -> quantity);
+    printf("   pre-enqueue-limit: %d\n", queue -> limit);
+    queue -> enqueue(queue, val);
+    printf("\n");
+    printf("   state of queue: \n");
+    queue -> print(queue);
+    printf("\n");
+    printf("   post-enqueue-quantity: %d\n", queue -> quantity);
+    printf("   post-enqueue-limit: %d\n", queue -> limit);
+    printf("   Space reserved for array: %lu\n", malloc_usable_size( queue -> collection));
+    printf("! end of enqueue\n");
+    printf("\n\n\n");
+}
 
+void test_dequeue(struct IntegerQueue* queue){
+    printf("Dequeue operation: \n");
+    printf("   pre-dequeue-quantity: %d\n", queue -> quantity);
+    printf("   pre-dequeue-limit: %d\n", queue -> limit);
+    printf("\n");
+    int val = queue -> dequeue(queue);
+    printf("   Value of dequeue operation: %d\n", val);
+    printf("   state of queue: \n");
+    queue -> print(queue);
+    printf("\n");
+    printf("   post-dequeue-quantity: %d\n", queue -> quantity);
+    printf("   post-dequeue-limit: %d\n", queue -> limit);
+    printf("   Space reserved for array: %lu\n", malloc_usable_size( queue -> collection));
+    printf("! end of dequeue\n");
+    printf("\n\n\n");
+}
+
+void test_peek(struct IntegerQueue* queue){
+    printf("Peek operation: \n");
+    printf("    Value at front: %d\n", queue -> peek(queue));
+    printf("! end of peek\n");
+    printf("\n");
+}
+
+int main(){
+    struct IntegerQueue* test_queue = create_IntegerQueue(5);
+    test_queue -> print(test_queue);
+
+    test_enqueue(test_queue,3);
+    test_enqueue(test_queue,10);
+    test_peek(test_queue);
+    test_dequeue(test_queue);
+    test_enqueue(test_queue,12);
+    test_enqueue(test_queue,15);
+    print_collection(test_queue);
+    test_enqueue(test_queue,18);
+    test_enqueue(test_queue,21);
+    test_enqueue(test_queue,34);
+    test_enqueue(test_queue,42);
+    test_enqueue(test_queue,50);
+    test_enqueue(test_queue,51);
+    test_enqueue(test_queue,52);
+    test_peek(test_queue);
+    print_collection(test_queue);
+    test_dequeue(test_queue);
+    print_collection(test_queue);
+    test_dequeue(test_queue);
+    print_collection(test_queue);
+    test_dequeue(test_queue);
+    test_enqueue(test_queue,53);
+    test_enqueue(test_queue,54);
+    test_enqueue(test_queue,55);
+    test_enqueue(test_queue,56);
+    print_collection(test_queue);
+    test_enqueue(test_queue,57);
+    print_collection(test_queue);
+    test_enqueue(test_queue,58);
+    print_collection(test_queue);
+    test_enqueue(test_queue,59);
+    print_collection(test_queue);
+    test_enqueue(test_queue,60);
+    print_collection(test_queue);
+    test_enqueue(test_queue,61);
     return 0;
 }
