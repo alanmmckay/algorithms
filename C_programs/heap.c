7f00ba955bf0872d6bebf32de5f912881d55ee03
Alan <alanmmckay@gmail.com>
2023-11-30 16:52:28 -0600

    Progress on min-heap implementation. I want to add comments to better describe what's happening with respect to the percolation methods.

--- C_programs/heap.c
+++ C_programs/heap.c
@@ -7,9 +7,14 @@ struct MinIntegerHeap{
     int quantity;
     int limit;
     int *collection;
+    void (*print)(struct MinIntegerHeap*);
+    void (*percup)(struct MinIntegerHeap*, int);
+    void (*percdown)(struct MinIntegerHeap*, int);
+    int (*remove)(struct MinIntegerHeap*);
+    void (*add)(struct MinIntegerHeap*, int);
 };
 
-void percup(struct MinIntegerHeap* this, int current_index){
+void percolate_up_MinIntegerHeap(struct MinIntegerHeap* this, int current_index){
     int parent_index = current_index / 2;
     int temp;
     while(current_index > 1 && this -> collection[current_index] < this -> collection[parent_index]){
@@ -33,39 +38,94 @@ int min_child(struct MinIntegerHeap* this, int index){
     }
 }
 
-void percdown(struct MinIntegerHeap* this, int index){
+void percolate_down_MinIntegerHeap(struct MinIntegerHeap* this, int index){
     bool exit_switch = false;
     int current_index = index;
     int min_child_index;
     int temp;
-    while((current_index * 2 <= this -> quantity) && (exit_switch == false)){
+    while((current_index * 2 < this -> quantity) && (exit_switch == false)){
         min_child_index = min_child(this,current_index);
-        if( this -> collection[current_index] >= min_child_index){
+        if( this -> collection[current_index] >= this -> collection[min_child_index]){
             temp = this -> collection[current_index];
             this -> collection[current_index] = this -> collection[min_child_index];
             this -> collection[min_child_index] = temp;
-            current_index = min_child_index;
-        }else{
-            exit_switch = true;
         }
+        current_index = min_child_index;
     }
 }
 
-int delete_min(struct MinIntegerHeap* this){
+int delete_MinIntegerHeap(struct MinIntegerHeap* this){
     int return_value = this -> collection[1];
     this -> collection[1] = this -> collection[ this -> quantity];
     this -> quantity = this -> quantity - 1;
-    percdown(this,1);
+    this -> percdown(this,1);
     return return_value;
 }
 
-void insert(struct MinIntegerHeap* this, int value){
+void insert_MinIntegerHeap(struct MinIntegerHeap* this, int value){
     this -> quantity = this -> quantity + 1;
     this -> collection[ this -> quantity] = value;
-    percup(this, this -> quantity);
+    this -> percup(this, this -> quantity);
+}
+
+void print_MinIntegerHeap(struct MinIntegerHeap* this){
+    for(int i = 1; i <= this -> quantity; i++){
+        printf("    %d \n", this -> collection[i]);
+    }
+    printf("\n\n\n");
+}
+
+struct MinIntegerHeap* create_MinIntegerHeap(int new_limit){
+    struct MinIntegerHeap* newHeap = (struct MinIntegerHeap*)malloc((new_limit * sizeof(int)) + sizeof(struct MinIntegerHeap));
+    if(newHeap){
+        newHeap -> limit = new_limit;
+        newHeap -> quantity = 0;
+        newHeap -> collection = (int*)calloc(new_limit, sizeof(int));
+        newHeap -> add = insert_MinIntegerHeap;
+        newHeap -> remove = delete_MinIntegerHeap;
+        newHeap -> percup = percolate_up_MinIntegerHeap;
+        newHeap -> percdown = percolate_down_MinIntegerHeap;
+        newHeap -> print = print_MinIntegerHeap;
+    }
+    return newHeap;
 }
 
 int main(){
-    printf("%d\n", 5 / 2);
+    struct MinIntegerHeap* test_heap = create_MinIntegerHeap(12);
+    test_heap -> add(test_heap, 5);
+    test_heap -> print(test_heap);
+    test_heap -> add(test_heap, 3);
+    test_heap -> print(test_heap);
+    test_heap -> add(test_heap, 4);
+    test_heap -> print(test_heap);
+    test_heap -> add(test_heap, 1);
+    test_heap -> print(test_heap);
+    printf("remove: %d\n",test_heap -> remove(test_heap));
+    test_heap -> print(test_heap);
+    printf("remove: %d\n",test_heap -> remove(test_heap));
+    test_heap -> print(test_heap);
+    printf("remove: %d\n",test_heap -> remove(test_heap));
+    test_heap -> print(test_heap);
+    printf("remove: %d\n",test_heap -> remove(test_heap));
+    test_heap -> print(test_heap);
+
+    for(int i = 1; i < 10; i ++){
+        test_heap -> add(test_heap, i * 2);
+        test_heap -> print(test_heap);
+    }
+
+    test_heap -> add(test_heap, 3);
+    test_heap -> print(test_heap);
+    test_heap -> add(test_heap, 9);
+    test_heap -> print(test_heap);
+    for(int i = 1; i < 10; i ++){
+        printf("removed: %d\n",test_heap -> remove(test_heap));
+        test_heap -> print(test_heap);
+
+    }
+        printf("removed: %d\n",test_heap -> remove(test_heap));
+        test_heap -> print(test_heap);
+        printf("removed: %d\n",test_heap -> remove(test_heap));
+        test_heap -> print(test_heap);
     return 0;
 }
